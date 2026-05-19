d3a70a2c9351aa3c1df809161ed4a6a9355527c1
trob8193-glitch <trob8193@gmail.com>
2026-05-19 16:54:18 -0500

    Export Evo LLM training orchestrator

--- src/core/evo-llm/index.js
+++ src/core/evo-llm/index.js
@@ -2,3 +2,5 @@ export * from './EvoLlmPolicy.js';
 export * from './EvoLlmPaths.js';
 export * from './EvoLlmDataset.js';
 export * from './EvoLlmEvaluation.js';
+export * from './EvoLlmOrchestratorPlan.js';
+export * from './EvoLlmOrchestratorRun.js';
