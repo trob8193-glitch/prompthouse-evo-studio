52eec7b3aa0199a0a295781f83462911f50eb4eb
trob8193-glitch <trob8193@gmail.com>
2026-05-27 04:27:43 -0500

    Fix Omni Evo Git import path

--- src/core/daemons/omni/OmniOrchestrator.mjs
+++ src/core/daemons/omni/OmniOrchestrator.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { writeDaemonReceipt, createEvoGitSnapshot } from '../../egit/index.js';
+import { writeDaemonReceipt, createEvoGitSnapshot } from '../../../egit/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
