c0dbe577068e48b74d90d4b02cf793640f329f55
trob8193-glitch <trob8193@gmail.com>
2026-05-27 04:24:18 -0500

    Add Evo Git public exports

---
+++ src/core/egit/index.js
@@ -0,0 +1,7 @@
+export * from './EvoGitPaths.js';
+export * from './EvoGitObjectStore.js';
+export * from './EvoGitRegistry.js';
+export * from './EvoGitAdapters.js';
+export * from './EvoGitApiAdapters.js';
+export * from './EvoGitSnapshot.js';
+export * from './EvoGitDaemonReceipts.js';
